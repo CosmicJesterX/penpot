9d70d9c2bc8c3f61237e5b91e94114304f76c621
luisddm <luis.dedios@kaleidos.net>
2025-01-17 14:30:08 +0100

    :recycle: Some code improvements

--- frontend/src/app/main/ui/comments.cljs
+++ frontend/src/app/main/ui/comments.cljs
@@ -598,7 +598,7 @@
 (mf/defc comment-reply-form*
   {::mf/props :obj
    ::mf/private true}
-  [{:keys [thread]}]
+  [{:keys [on-submit]}]
   (let [show-buttons? (mf/use-state false)
         content       (mf/use-state "")
 
@@ -619,14 +619,15 @@
 
         on-cancel
         (mf/use-fn
-         #(do (reset! content "")
-              (reset! show-buttons? false)))
-
-        on-submit
-        (mf/use-fn
-         (mf/deps thread @content)
          (fn []
-           (st/emit! (dcm/add-comment thread @content))
+           (reset! content "")
+           (reset! show-buttons? false)))
+
+        on-submit*
+        (mf/use-fn
+         (mf/deps @content)
+         (fn []
+           (on-submit @content)
            (on-cancel)))]
 
     [:div {:class (stl/css :form)}
@@ -643,10 +644,12 @@
        [:div {:class (stl/css :form-buttons-wrapper)}
         [:> mentions-button*]
         [:> button* {:variant "ghost"
+                     :type "button"
                      :on-click on-cancel}
          (tr "ds.confirm-cancel")]
         [:> button* {:variant "primary"
-                     :on-click on-submit
+                     :type "button"
+                     :on-click on-submit*
                      :disabled disabled?}
          (tr "labels.post")]])]))
 
@@ -655,6 +658,9 @@
   [{:keys [content on-submit on-cancel]}]
   (let [content (mf/use-state content)
 
+        disabled? (or (str/blank? @content)
+                      (str/empty? @content))
+
         on-change
         (mf/use-fn
          #(reset! content %))
@@ -662,10 +668,8 @@
         on-submit*
         (mf/use-fn
          (mf/deps @content)
-         (fn [] (on-submit @content)))
-
-        disabled? (or (str/blank? @content)
-                      (str/empty? @content))]
+         (fn []
+           (on-submit @content)))]
 
     [:div {:class (stl/css :form)}
      [:> comment-input*
@@ -677,9 +681,11 @@
      [:div {:class (stl/css :form-buttons-wrapper)}
       [:> mentions-button*]
       [:> button* {:variant "ghost"
+                   :type "button"
                    :on-click on-cancel}
        (tr "ds.confirm-cancel")]
       [:> button* {:variant "primary"
+                   :type "button"
                    :on-click on-submit*
                    :disabled disabled?}
        (tr "labels.post")]]]))
@@ -719,7 +725,8 @@
         on-submit
         (mf/use-fn
          (mf/deps draft)
-         (partial on-submit draft))]
+         (fn []
+           (on-submit draft)))]
 
     [:> (mf/provider mentions-context) {:value mentions-s}
      [:div
@@ -747,9 +754,11 @@
        [:div {:class (stl/css :form-buttons-wrapper)}
         [:> mentions-button*]
         [:> button* {:variant "ghost"
+                     :type "button"
                      :on-click on-esc}
          (tr "ds.confirm-cancel")]
         [:> button* {:variant "primary"
+                     :type "button"
                      :on-click on-submit
                      :disabled disabled?}
          (tr "labels.post")]]]
@@ -958,7 +967,13 @@
                         (->> (vals comments-map)
                              (sort-by :created-at)))
 
-        first-comment (first comments)]
+        first-comment (first comments)
+
+        on-submit
+        (mf/use-fn
+         (mf/deps thread)
+         (fn [content]
+           (st/emit! (dcm/add-comment thread content))))]
 
     (mf/with-effect [thread-id]
       (st/emit! (dcm/retrieve-comments thread-id)))
@@ -992,7 +1007,7 @@
            [:* {:key (dm/str (:id item))}
             [:> comment-floating-thread-item* {:comment item}]])]
 
-        [:> comment-reply-form* {:thread thread}]
+        [:> comment-reply-form* {:on-submit on-submit}]
 
         [:> mentions-panel*]])]))
 
